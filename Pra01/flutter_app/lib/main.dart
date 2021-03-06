996c9bc4ef9bcb3f7e2e619ba1bc6aa5a0a549cd
ansuvin <dkstnqls0925@naver.com>
2021-03-06 11:50:14 +0900

    [UPDATE] Drawer

--- Pra01/flutter_app/lib/main.dart
+++ Pra01/flutter_app/lib/main.dart
@@ -24,12 +24,6 @@ class MyPage extends StatelessWidget {
         title: Text("Appbar icon menu"),
         centerTitle: true,
         elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.menu),
-          onPressed: () {
-            print('menu button is clicked');
-          },
-        ),
         actions: [
           IconButton(
             icon: Icon(Icons.shopping_cart),
@@ -44,7 +38,76 @@ class MyPage extends StatelessWidget {
             },
           ),
         ],
-      )
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("assets/rainbow.jpg"),
+                backgroundColor: Colors.white,
+              ),
+              otherAccountsPictures: [
+                CircleAvatar(
+                  backgroundImage: AssetImage("assets/red.jpg"),
+                  backgroundColor: Colors.white,
+                ),
+                // CircleAvatar(
+                //   backgroundImage: AssetImage("assets/red.jpg"),
+                //   backgroundColor: Colors.white,
+                // ),
+                // CircleAvatar(
+                //   backgroundImage: AssetImage("assets/red.jpg"),
+                //   backgroundColor: Colors.white,
+                // ),
+              ],
+              accountName: Text("BBANTO"),
+              accountEmail: Text("bbanto@bbanto.com"),
+              onDetailsPressed: (){
+                print("arrow is clicked");
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40),
+                )
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home,
+              color: Colors.grey[850],),
+              title: Text("Home"),
+              onTap: (){
+                print("Home is clicked");
+              },
+              trailing: Icon(Icons.add,
+              color: Colors.grey[850],),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings,
+                color: Colors.grey[850],),
+              title: Text("setting"),
+              onTap: (){
+                print("setting is clicked");
+              },
+              trailing: Icon(Icons.add,
+                color: Colors.grey[850],),
+            ),
+            ListTile(
+              leading: Icon(Icons.question_answer,
+                color: Colors.grey[850],),
+              title: Text("Q&A"),
+              onTap: (){
+                print("Q&A is clicked");
+              },
+              trailing: Icon(Icons.add,
+                color: Colors.grey[850],),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
