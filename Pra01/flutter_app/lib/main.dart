c1153f28a26309ecad2827d244439ed099859405
ansuvin <dkstnqls0925@naver.com>
2021-03-06 12:19:07 +0900

    [UPDATE] SnackBar2

--- Pra01/flutter_app/lib/main.dart
+++ Pra01/flutter_app/lib/main.dart
@@ -20,30 +20,35 @@ class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Snack Bar"),
-          centerTitle: true,
-          elevation: 0,
-        ),
-        body: Builder(
-          builder: (BuildContext ctx) {
-            return Center(
-              child: FlatButton(
-                child: Text(
-                  "Show me",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-                color: Colors.red,
-                onPressed: () {
-                  Scaffold.of(ctx).showSnackBar(SnackBar(
-                    content: Text("Hello"),
-                  ));
-                },
-              ),
-            );
-          },
-        ));
+      appBar: AppBar(
+        title: Text("Snack Bar"),
+        centerTitle: true,
+      ),
+      body: MySnackBar(),
+    );
+  }
+}
+
+class MySnackBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text("show me"),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Hello",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+              ),),
+              backgroundColor: Colors.teal,
+              duration: Duration(milliseconds: 1000),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
