912034edaf433a162c28ea01f6658d4daaa193af
ansuvin <dkstnqls0925@naver.com>
2021-03-11 18:10:37 +0900

    [UPDATE] HTTP

--- Pra04/flutter_app/lib/screens/loading.dart
+++ Pra04/flutter_app/lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,27 +10,59 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getLociation() async {
-    Position position = await Geolocator.
-    getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getLociation();
+    fetchData();
   }
+
+  void getLociation() async {
+    try {
+      Position position = await Geolocator.
+      getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch(e) {
+      print("인터넷 연결에 문제가 있습니다.");
+    }
+  }
+
+  void fetchData() async {
+    var url = Uri.https("jsonplaceholder.typicode.com", "/posts");
+    http.Response response = await http.get(url);
+    if(response.statusCode == 200) {
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)[0]['title'];
+      print(myJson);
+    }
+  }
+
+  final items = List.generate(10,(i) => i).toList();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: RaisedButton(
-          onPressed: (){
-            getLociation();
-          },
-          child: Text("Get my location",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-          ),
-          color: Colors.blue,
-        ),
+        child: GridView.count(
+          crossAxisCount: 4,
+          children: items.map((e) => Container(
+            child: Text("$e"),
+            width: 100,
+            height: 100,
+            color: Colors.blue,
+            margin: EdgeInsets.all(16),
+          )).toList(),
+        )
+        // RaisedButton(
+        //   onPressed: null,
+        //   child: Text("Get my location",
+        //   style: TextStyle(
+        //     color: Colors.white,
+        //   ),
+        //   ),
+        //   color: Colors.blue,
+        // ),
       ),
     );
   }
