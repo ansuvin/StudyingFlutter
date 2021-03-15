44dff6d7a741a338a3dd3d44a4e9ab9f455930b1
ansuvin <dkstnqls0925@naver.com>
2021-03-15 20:19:19 +0900

    [CREATE] Pra05

--- Pra04/flutter_app/lib/screens/loading.dart
+++ Pra04/flutter_app/lib/screens/loading.dart
@@ -34,19 +34,17 @@ class _LoadingState extends State<Loading> {
 
     var data = await network.getJsonData();
 
-    // Navigator.push(context, MaterialPageRoute(builder: (context){
-    //   return WeatherScreen(parseData: data);
-    // }));
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+      return WeatherScreen(parseData: data);
+    }));
   }
-
-  final items = List.generate(10,(i) => i).toList();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: '메뉴3', title: ),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: '메뉴3'),
           BottomNavigationBarItem(icon: Icon(Icons.info), label: '메뉴'),
           BottomNavigationBarItem(icon: Icon(Icons.error), label: '메뉴2')
         ],
