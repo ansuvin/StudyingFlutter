1a60225a00fb632c65c7fbce45f4695c72ce3aaf
ansuvin <dkstnqls0925@naver.com>
2021-03-16 20:55:54 +0900

    [FINISH] StopWatch

--- Pra07/flutter_app/lib/main.dart
+++ Pra07/flutter_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -6,10 +7,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "StopWatch",
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: StopWatchPage(),
     );
   }
@@ -21,9 +21,132 @@ class StopWatchPage extends StatefulWidget {
 }
 
 class _StopWatchPageState extends State<StopWatchPage> {
+  Timer _timer;
+
+  var _time = 0;
+  var _isRunning = false;
+
+  List<String> _lapTimes = [];
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("StopWatch"),
+      ),
+      body: _buildBody(),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 50,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {
+          _clickButton();
+        }),
+        child: _isRunning ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  Widget _buildBody() {
+    var sec = _time ~/100;  //초
+    var hundredth = '${_time % 100}'.padLeft(2,"0");  // 1/100초
+
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 50),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "$sec",
+                      style: TextStyle(
+                        fontSize: 50,
+                      ),
+                    ),
+                    Text("$hundredth"),
+                  ],
+                ),
+                Container(
+                  // 랩타임을 표시하는 영역
+                  width: 100,
+                  height: 200,
+                  child: ListView(
+                    children: _lapTimes.map((time) => Text(time)).toList(),
+                  ),
+                )
+              ],
+            ),
+            Positioned(
+                left: 10,
+                bottom: 10,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.deepOrange,
+                  onPressed: _reset,
+                  child: Icon(Icons.rotate_left),
+                )),
+            Positioned(
+                right: 10,
+                bottom: 10,
+                child: RaisedButton(
+                  onPressed:() {
+                    setState(() {
+                      _recordLapTime("$sec.$hundredth");
+                    });
+                  },
+                  child: Text("랩타임"),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _clickButton() {
+    _isRunning = !_isRunning;
+
+    if(_isRunning) {
+      _start();
+    } else {
+      _pause();
+    }
+  }
+
+  void _start() {
+    _timer = Timer.periodic(Duration(milliseconds: 10), (timer) {
+      setState(() {
+        _time++;
+      });
+    });
+  }
+
+  void _pause() {
+    _timer?.cancel();
+  }
+
+  void _reset() {
+    setState(() {
+      _isRunning = false;
+      _timer?.cancel();
+      _lapTimes.clear();
+      _time = 0;
+    });
+  }
+
+  void _recordLapTime(String time) {
+    _lapTimes.insert(_lapTimes.length, "${_lapTimes.length +1}등 $time");
   }
 }
-
