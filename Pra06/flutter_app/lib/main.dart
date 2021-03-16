6672d597869145ebb20f450958835fd5cac9c394
ansuvin <dkstnqls0925@naver.com>
2021-03-16 19:57:36 +0900

    [UPDATE] TextField validator

--- Pra06/flutter_app/lib/main.dart
+++ Pra06/flutter_app/lib/main.dart
@@ -21,6 +21,16 @@ class _BmiMainState extends State<BmiMain> {
 
   final _formKey = GlobalKey<FormState>();
 
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,10 +44,46 @@ class _BmiMainState extends State<BmiMain> {
           child: Column(
             children: [
               TextFormField(
+                controller: _heightController,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   hintText: "키"
-                )
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.trim().isEmpty) { // 입력한 값의 앞뒤 공백을 제거한 것이 비었다면 에러표시
+                    return "키를 입력하세요";
+                  }
+                  return null;  // null을 반환하면 에러 없음
+                },
+              ),
+              SizedBox(height: 16,),
+              TextFormField(
+                controller: _weightController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: '몸무게',
+                ),
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if(value.trim().isEmpty) {
+                    return "몸무게를 입력하세요.";
+                  }
+                  return null;
+                },
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16),
+                alignment: Alignment.centerRight,
+                child: RaisedButton(
+                  onPressed: () {
+                    // 폼에 입력된 값 검증
+                    if (_formKey.currentState.validate()){
+                      // 검증시 처리
+                    }
+                  },
+                  child: Text("결과"),
+                ),
               )
             ],
           ),
