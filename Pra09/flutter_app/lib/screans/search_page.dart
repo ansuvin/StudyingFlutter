f2dcafec7510e67e3c118abdc65cfac48757461f
ansuvin <dkstnqls0925@naver.com>
2021-03-27 17:40:19 +0900

    [CREATE] button.dart

--- Pra09/flutter_app/lib/screans/search_page.dart
+++ Pra09/flutter_app/lib/screans/search_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/widgets/app_bar.dart';
+import 'package:flutter_app/widgets/button.dart';
 import 'package:flutter_app/widgets/text_field.dart';
 
 class SearchApp extends StatelessWidget {
@@ -78,18 +79,15 @@ class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: key,
-      appBar: buildAppBar("이건 AppBar"),
-      body: buildDropDownTextField()
-    );
+        key: key,
+        appBar: buildAppBar("이건 AppBar"),
+        body: buildDropDownTextField());
   }
 
   List<ChildItem> _buildSearchList() {
     if (_searchText.isEmpty) {
-      print("_buildSearchList true");
       return _list.map((contact) => ChildItem(contact)).toList();
     } else {
-      print("buildSearchList false");
       List<String> _searchList = [];
       for (int i = 0; i < _list.length; i++) {
         String name = _list.elementAt(i);
@@ -111,30 +109,91 @@ class _SearchPageState extends State<SearchPage> {
         Expanded(
           child: _IsSearching
               ? Container(
-            width: 350,
-            margin: EdgeInsets.only(bottom: 16),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 10.0,
-                      offset: Offset(0, 5),
-                      color: Colors.grey[500])
-                ]),
-            child: ListView(
-              padding: EdgeInsets.symmetric(vertical: 8),
-              children: _buildSearchList(),
-            ),
-          )
+                  width: 350,
+                  margin: EdgeInsets.only(bottom: 20),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10.0,
+                            offset: Offset(0, 5),
+                            color: Colors.grey[500])
+                      ]),
+                  child: ListView(
+                    padding: EdgeInsets.symmetric(vertical: 8),
+                    children: _buildSearchList(),
+                  ),
+                )
               : SizedBox(),
-        )
+        ),
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: SizedBox(
+              height: 45,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Text(
+                      "태그",
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Container(
+                    height: 1,
+                    width: 360,
+                    color: Colors.grey[500],
+                    margin: EdgeInsets.only(bottom: 5, top: 5),
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: 5,
+                          itemBuilder: (context, index) {
+                            return buildItemTag(index);
+                          }),
+                    ),
+                  ),
+                ],
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: makeBtn(
+            msg: "조회하기",
+            onPressed: () => print("누른다 버튼"),
+            mode: 2,
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ],
     );
   }
+
+  Widget buildItemTag(int index) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(5, 1, 5, 1),
+      margin: EdgeInsets.only(right: 8),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.blue[400],
+          )),
+      child: Text(
+        "#${_list[index]}",
+        style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
+      ),
+    );
+  }
 }
-
-
 
 class ChildItem extends StatelessWidget {
   final String name;
