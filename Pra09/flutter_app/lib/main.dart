8b066d1851fc746c90458f11d3d8229d73c6c46e
ansuvin <dkstnqls0925@naver.com>
2021-03-29 20:55:47 +0900

    [ADD] Notice List

--- Pra09/flutter_app/lib/main.dart
+++ Pra09/flutter_app/lib/main.dart
@@ -39,6 +39,20 @@ class _MyHomePageState extends State<MyHomePage> {
     SliderCard(title: "호잇", image: "images/loco.jpg", route: "/"),
   ];
 
+  List<Notification> notiList = [];
+
+  initNotiList() {
+    for(int i=0;i<10;i++) {
+      notiList.add(Notification("${i}.title", "내용입니다", "2021.03.19", List.generate(6, (index) => "${index}태그")));
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initNotiList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,6 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Container(
         color: Colors.white,
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Padding(
               padding: const EdgeInsets.fromLTRB(26, 18, 26, 18),
@@ -72,7 +87,9 @@ class _MyHomePageState extends State<MyHomePage> {
                       )
                     ],
                   ),
-                  SizedBox(height: 3,),
+                  SizedBox(
+                    height: 3,
+                  ),
                   Text(
                     "취업 준비 해요",
                     style: TextStyle(
@@ -84,47 +101,148 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             makeSlider(sliderList),
+            Container(
+              height: 12,
+              color: Color(0xffEFEFEF),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(25, 16, 25, 16),
+              child: Text(
+                "공지사항",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: notiList.length,
+                itemBuilder: (context, index) {
+                  return buildItemCompany(context, index);
+                }, 
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,),
+            )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget buildItemCompany(BuildContext context, int index) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+      elevation: 5,
+      margin: EdgeInsets.fromLTRB(25, 13, 25, 13),
+      child: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    "${notiList[index].title}",
+                    style: TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.w900),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 6, bottom: 6),
+              child: Container(
+                width: 331,
+                height: 54,
+                child: Text(
+                  "${notiList[index].content}. 이것은 공지사나? 이야야야야야야야야그럼 안되는디dksl",
+                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500,),
+                overflow: TextOverflow.ellipsis,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: SizedBox(
+                      height: 22,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemCount: 5,
+                          itemBuilder: (context, index) {
+                            return buildItemTag(index);
+                          })),
+                ),
+                Text(
+                  "등록일: ${notiList[index].date}",
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w400),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildItemTag(int index) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(5, 1, 5, 1),
+      margin: EdgeInsets.only(right: 8),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(
+            color: Colors.blue[400],
+          )),
+      child: Text(
+        "#${notiList[index].tag[index]}",
+        style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
       ),
     );
   }
 }
 
 Widget makeSlider(List<SliderCard> list) {
-  return CarouselSlider(
-      items: list.map((card) {
-        return Builder(builder: (BuildContext context) {
-          return Container(
-            width: 357,
-            height: 210,
-            margin: EdgeInsets.only(
-                left: 10, right:10, top: 10, bottom: 10),
-            decoration: BoxDecoration(
-                color: Colors.black54,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(0, 2),
-                      blurRadius: 3,
-                      spreadRadius: 2)
-                ]),
-            child: Center(
-                child: Text(
-                  card.title,
-                  style: TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.w800, color: Colors.white),
-                )),
-          );
-        });
-      }).toList(),
-      options: CarouselOptions(
-        autoPlayAnimationDuration: Duration(seconds: 2),
-        autoPlayInterval: Duration(seconds: 3),
-        autoPlay: true,
-        height: 150,
-      ));
+  return Padding(
+    padding: const EdgeInsets.only(top: 10, bottom: 30),
+    child: CarouselSlider(
+        items: list.map((card) {
+          return Builder(builder: (BuildContext context) {
+            return Container(
+              width: 357,
+              height: 250,
+              margin: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
+              decoration: BoxDecoration(
+                  color: Colors.black54,
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0, 2),
+                        blurRadius: 3,
+                        spreadRadius: 2)
+                  ]),
+              child: Center(
+                  child: Text(
+                card.title,
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white),
+              )),
+            );
+          });
+        }).toList(),
+        options: CarouselOptions(
+          autoPlayAnimationDuration: Duration(seconds: 2),
+          autoPlayInterval: Duration(seconds: 3),
+          autoPlay: true,
+          height: 150,
+        )),
+  );
 }
 
 class SliderCard {
@@ -134,4 +252,13 @@ class SliderCard {
 
   SliderCard(
       {@required this.title, @required this.image, @required this.route});
+}
+
+class Notification{
+  String title;
+  String content;
+  String date;
+  List<String> tag;
+
+  Notification(this.title, this.content, this.date, this.tag);
 }
