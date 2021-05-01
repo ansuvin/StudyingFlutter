76f3ee602b10bdf51a038ebfb8a9a044c994196d
ansuvin <dkstnqls0925@naver.com>
2021-05-01 23:49:14 +0900

    Practice flutter_string_encryption

--- Pra09/flutter_app/lib/screans/login.dart
+++ Pra09/flutter_app/lib/screans/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/screans/logout.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_string_encryption/flutter_string_encryption.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class _LoginPageState extends State<LoginPage> {
   TextEditingController idController;
   TextEditingController passController;
   String userInfo = "";
+  final cryptor = PlatformStringCryptor();
 
   static final storage = new FlutterSecureStorage();
 
@@ -19,6 +21,7 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
     idController = TextEditingController();
     passController = TextEditingController();
+
 
     // 비동기로 flutter secure storage 정보를 불러오기
     WidgetsBinding.instance.addPostFrameCallback((_) {
@@ -61,6 +64,11 @@ class _LoginPageState extends State<LoginPage> {
               decoration: InputDecoration(labelText: "password"),
             ),
             RaisedButton(onPressed: () async {
+              final password = "1234";
+              final String salt = await cryptor.generateSalt();
+              final String key = await cryptor.generateKeyFromPassword(password, salt);
+              final String encrypted = await cryptor.encrypt(passController.text, key);
+              print("encrypted: $encrypted");
               await storage.write(key: "login",
                   value: "id " + idController.text.toString() + " " +
                       "password " + passController.text.toString());
@@ -68,7 +76,7 @@ class _LoginPageState extends State<LoginPage> {
               Navigator.pushReplacement(context, MaterialPageRoute(
                   builder: (context) =>
                       LogOutPage(
-                        id: idController.text, pass: passController.text,)));
+                        id: idController.text, pass: encrypted, keyStr: key)));
             },
               child: Text("로그인"),)
           ],
--- Pra09/flutter_app/lib/screans/logout.dart
+++ Pra09/flutter_app/lib/screans/logout.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/screans/login.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_string_encryption/flutter_string_encryption.dart';
 
 class LogOutPage extends StatefulWidget {
   String id;
   String pass;
+  String keyStr;
 
-  LogOutPage({this.id, this.pass});
+  LogOutPage({this.id, this.pass, this.keyStr});
 
   @override
   _LogOutPageState createState() => _LogOutPageState();
@@ -14,6 +16,9 @@ class LogOutPage extends StatefulWidget {
 
 class _LogOutPageState extends State<LogOutPage> {
   static final storage = FlutterSecureStorage();
+  final cryptor = PlatformStringCryptor();
+
+  var flag = true;
 
   String id;
   String pass;
@@ -23,6 +28,31 @@ class _LogOutPageState extends State<LogOutPage> {
     super.initState();
     id = widget.id;
     pass = widget.pass;
+  }
+
+  // flag == true
+  decrypted() async {
+    try {
+      final String decrypted =
+          await cryptor.decrypt(widget.pass, widget.keyStr);
+      print("decrypted: $decrypted");
+      setState(() {
+        pass = decrypted;
+        flag = !flag;
+      });
+    } catch (e) {
+      print("error: $e");
+    }
+  }
+
+  // flag == false
+  encrypted() async {
+    final String encrypted = await cryptor.encrypt(widget.pass, widget.keyStr);
+    setState(() {
+      pass = encrypted;
+      flag = !flag;
+    });
+    print("encrypted: $encrypted");
   }
 
   @override
@@ -45,6 +75,10 @@ class _LogOutPageState extends State<LogOutPage> {
                     MaterialPageRoute(builder: (context) => LoginPage()));
               },
               child: Text("Logout"),
+            ),
+            RaisedButton(
+              onPressed: flag ? decrypted : encrypted,
+              child: Text(flag ? "복호화" : "암호화"),
             )
           ],
         ),
