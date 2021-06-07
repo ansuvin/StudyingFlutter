a1c90764bd1ae30171bfe4ec7a76e4a070cb2116
ansuvin <dkstnqls0925@naver.com>
2021-06-07 11:08:54 +0900

    [UPDATE] Pagination - insert

--- Pra11/flutter_app/lib/main.dart
+++ Pra11/flutter_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/model/post_vo.dart';
 import 'package:flutter_app/model/retrofit_helper.dart';
 
 void main() {
@@ -32,9 +31,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   RetrofitHelper helper;
-  List<PostVO> postList = [];
-  final showItemCount = 8;
-  int itemCount = 8;
+  List<String> postList = [];
+  final showItemCount = 10;
+  int itemCount = 10;
+  int cnt = 15;
 
   final ScrollController _scrollController = ScrollController();
 
@@ -79,91 +79,114 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: FutureBuilder(
-              future: _getPosts(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  postList = snapshot.data;
-                  return ListView.builder(
-                      controller: _scrollController,
-                      itemCount: itemCount + 1,
-                      itemBuilder: (context, index) {
-                        print(
-                            "index: $index, postList.length: ${postList.length}");
-                        if (index == itemCount) {
-                          if (index == postList.length) {
-                            return Card(
-                              child: Center(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(15),
-                                  child: RaisedButton(
-                                    child: Text("맨 처음으로"),
-                                    onPressed: () {
-                                      print("처음으로!!");
-                                      _scrollController.animateTo(
-                                          _scrollController
-                                              .position.minScrollExtent,
-                                          duration: Duration(milliseconds: 200),
-                                          curve: Curves.elasticOut);
-                                    },
-                                  ),
-                                ),
-                              ),
-                            );
-                          }
-                          return Card(
-                            child: Center(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.only(top: 15, bottom: 15),
-                                child: CircularProgressIndicator(),
-                              ),
-                            ),
-                          );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          children: [
+            RaisedButton(
+              onPressed: () {
+                postList.add("추가됨 ${postList.length}");
+                setState(() {
+                  _getPosts();
+                  itemCount ++;
+                  cnt ++;
+                });
+                print("itemCount: $itemCount, postList: ${postList.length}");
+              },
+              child: Text("추가하기"),
+            ),
+            Expanded(
+                child: FutureBuilder(
+                    future: _getPosts(),
+                    builder: (BuildContext context, AsyncSnapshot snapshot) {
+                      if (snapshot.hasData) {
+                        postList = snapshot.data;
+                        if (postList.length <= showItemCount) {
+                          itemCount = postList.length;
                         }
-                        return itemPost(context, index);
-                      });
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              })),
-    );
+                        return ListView.builder(
+                            controller: _scrollController,
+                            itemCount: itemCount + 1,
+                            itemBuilder: (context, index) {
+                              print(
+                                  "itemCount: $itemCount, index: $index, postList.length: ${postList.length}");
+                              if (index == itemCount) {
+                                if (index == 0) {
+                                  return Card(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(18)),
+                                    elevation: 5,
+                                    margin: EdgeInsets.fromLTRB(25, 13, 25, 13),
+                                    child: Center(
+                                      child: Padding(
+                                          padding: EdgeInsets.all(10),
+                                          child: Text(
+                                            "등록된 태그가 없습니다.",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w700),
+                                          )),
+                                    ),
+                                  );
+                                } else if (index == postList.length) {
+                                  return Card(
+                                    child: Center(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(15),
+                                        child: RaisedButton(
+                                          child: Text("맨 처음으로"),
+                                          onPressed: () {
+                                            print("처음으로!!");
+                                            _scrollController.animateTo(
+                                                _scrollController
+                                                    .position.minScrollExtent,
+                                                duration:
+                                                    Duration(milliseconds: 200),
+                                                curve: Curves.elasticOut);
+                                          },
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                } else {
+                                  return Card(
+                                    child: Center(
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 15, bottom: 15),
+                                        child: CircularProgressIndicator(),
+                                      ),
+                                    ),
+                                  );
+                                }
+                              }
+                              return itemPost(context, index);
+                            });
+                      } else {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    })),
+          ],
+        ));
   }
 
-  Future<List<PostVO>> _getPosts() async {
-    try {
-      var res = await helper.getPosts();
-      if (res != null) {
-        return res.toList();
-      } else {
-        return null;
-      }
-    } catch (e) {
-      print("error: $e");
-    }
+  Future<List<String>> _getPosts() async {
+    var list = List.generate(cnt, (index) => "하이하이 $index");
+    return list;
   }
 
   Widget itemPost(BuildContext context, int index) {
     return Card(
       clipBehavior: Clip.antiAlias,
       child: Padding(
-        padding: EdgeInsets.all(15),
+        padding: EdgeInsets.all(20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("작성자: ${postList[index].userId}, 번호: ${postList[index].id}"),
-            Text("제목: ${postList[index].title}"),
-            Text(
-              "내용: ${postList[index].body}",
-              maxLines: 3,
-              overflow: TextOverflow.ellipsis,
-            )
+            Text("작성자: ${postList[index]},"),
           ],
         ),
       ),
